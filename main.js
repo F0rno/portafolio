1c98a747d0589aff5a175cba7b93ddf00a8b1730
F0rno <pfornell@iesrafaelalberti.es>
2024-05-06 22:07:29 +0200

    loading screen, no trigger for real load

---
+++ main.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+    const canvas = document.querySelector('#defaultCanvas0');
+    canvas.parentNode.removeChild(canvas);
+}, 1000);
+
+/*
+window.onload = function() {
+    if (window.sketch) {
+        console.log("p5.min.js has been loaded.");
+    } else {
+        console.log("p5.min.js has not been loaded.");
+    }
+};
+*/
