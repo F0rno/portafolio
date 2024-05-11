c17c58d6de47de461f0e3d5c72a4fb4d54a87018
F0rno <pfornell@iesrafaelalberti.es>
2024-05-11 19:24:48 +0200

    trying to fix the prods paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    base: 'https://f0rno.github.io/portafolio/',
+    base: '/portafolio/',
   },
 })
