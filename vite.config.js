a377271ac67cec3a865f0f7f1b05d787cee540b2
F0rno <pfornell@iesrafaelalberti.es>
2024-05-11 17:15:41 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 export default defineConfig({
   build: {
     outDir: 'dist',
-    base: 'https://f0rno.github.io/portafolio/',
+    base: '/portafolio/',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
